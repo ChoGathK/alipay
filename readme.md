c3f61d790af905ff0d5ae5d2ff0f2f6715867b7f
刘阳 <chogath@163.com>
2020-09-27 16:26:09 +0800

    feat: init

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# JiaYangStudio/AliPay
+# AliPay
 
 [AliPay-Sdk](https://github.com/alipay/alipay-sdk-nodejs-all) 的 TypeScript 封装，为您的支付服务提供更简洁的 API
 
---
+++ template/git.push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 捕捉执行异常
+echo "-----本次推送内容的标题是: ${1}-----"
+
+function error_exit {
+  echo "$1" 1>&2
+  exit 1
+}
+
+# yarn lint || error_exit "lint"
+
+# yarn build || error_exit "tsc"
+
+git add . || error_exit "git add"
+
+git commit -m "${1}" || error_exit "git commit"
+
+git push || error_exit "git push"
+
+echo "-----已同步代码至远程-----"
